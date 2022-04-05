d9419040e0fe0c8cb91277b7b15d165a2fab5126
Ponja94 <henrique.danielb@gmail.com>
2022-04-05 18:39:23 -0300

    third commit

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ var btnAdd = document.getElementById('btnAdd');
 btnAdd.addEventListener('click', addNewItem)
 enter.addEventListener('keypress', addNewItem);
 elementl.addEventListener('click', excludeItem);
-elementl.addEventListener('click', excludeItem);
 
 
 /* Add a new item on list */
@@ -28,18 +27,18 @@ function addNewItem(){
 			let newItem = listItem[i];
 
 
-			listDataToWriteInContainer += `<div class="container input-group d-inline" id="id${i}">
-	              <div class="row input-group-prepend mr-5"> <!-- -->
-	                <div class="col form-check input-group-text  justify-content-between">
+			listDataToWriteInContainer += `<div class="container input-group d-inline"">
+              <div class="row input-group-prepend mr-5"> <!-- -->
+                <div class="col form-check input-group-text justify-content-between">
 
-	                  <input class="float-left" type="checkbox" name="" id="flexCheck">
-	                  
-	                  <label class="form-check-label text-center" for="flexCheck" id="itemContent">${newItem}</label>
+                  <input class="float-left" type="checkbox" id="flexCheck">
+                  
+                  <label class="form-check-label text-center" for="flexCheck" id="itemContent">${newItem}</label>
 
-	                  <i class="btn fa-solid fa-trash-can" id="${i}"></i>
-	                </div>
-	              </div> <!-- -->
-	            </div>`;
+                  <i class="btn fa-solid fa-trash-can" id="${i}"></i>
+                </div>
+              </div>
+            </div>`;
 		}
 		event.target.value=""; /* Limpa o valor digitado anteriormente*/
 		document.getElementById('toDoList').innerHTML = listDataToWriteInContainer;
@@ -52,15 +51,20 @@ function addNewItem(){
 function excludeItem(){
 
 	const elemento = event.target;
+	const i = elemento.id; 
+	const classesElemento = elemento.classList;
 
-	i = elemento.id; 
-
-	console.log(i);
-	console.log(listItem[i])
-	listItem.splice(i,1);
-	console.log(listItem)
-	atualizar();
+	const toVerify = document.querySelector('i');
+	const classesVerif = toVerify.classList;
 	
+
+	if(classesElemento[1] == classesVerif[1]){
+		listItem.splice(i,1);
+		atualizar();
+
+	}else{
+		console.log('erro')
+	}
 }
 
 function atualizar(){
@@ -68,28 +72,21 @@ function atualizar(){
 	let listDataToWriteInContainer = '';
 	for(i in listItem){
 		let newItem = listItem[i];
-		listDataToWriteInContainer += `<div class="container input-group d-inline" id="id${i}">
+		listDataToWriteInContainer += `<div class="container input-group d-inline"">
               <div class="row input-group-prepend mr-5"> <!-- -->
-                <div class="col form-check input-group-text  justify-content-between">
+                <div class="col form-check input-group-text justify-content-between">
 
-                  <input class="float-left" type="checkbox" name="" id="flexCheck">
+                  <input class="float-left" type="checkbox" id="flexCheck">
                   
                   <label class="form-check-label text-center" for="flexCheck" id="itemContent">${newItem}</label>
 
                   <i class="btn fa-solid fa-trash-can" id="${i}"></i>
                 </div>
-              </div> <!-- -->
+              </div>
             </div>`;
 	}
 
 	document.getElementById('toDoList').innerHTML = listDataToWriteInContainer;
-
-
-}
-
-function comlpetedItem(){
-
-	alert('foi222');
 
 
 }
